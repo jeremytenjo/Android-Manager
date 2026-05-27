6fc7a74a666c2df68c6f0374f75f399ea11b5083
jeremytenjo <tenjojeremy@gmail.com>
2026-05-27 12:18:18 -0400

    Initial Commit

---
+++ Android Manager/Android_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Android_ManagerApp.swift
+//  Android Manager
+//
+//  Created by Jeremy Tenjo on 2026-05-27.
+//
+
+import SwiftUI
+
+@main
+struct Android_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
